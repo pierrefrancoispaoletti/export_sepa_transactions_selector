9dea5238a63177a71399ce9e7415f6f480de902e
pierrefrancoispaoletti <pierre.paoletti1@gmail.com>
2023-10-16 09:57:47 +0200

    modification de l'url appelée pour modifier sur charger le module , rajout d'un / dans l'url du fichier config

--- src/_config.js
+++ src/_config.js
@@ -1,8 +1,10 @@
 let URL_GENERATION = "";
 
+console.log(process.env.PUBLIC_URL);
+
 if (process.env.NODE_ENV === "production") {
   URL_GENERATION =
-    "./_DevClient/EXPORT_SEPA/traitements_php/generation/index.php";
+    "/_DevClient/EXPORT_SEPA/traitements_php/generation/index.php";
 } else {
   URL_GENERATION =
     "http://localhost:8080/EXPORT_SEPA/traitements_php/generation/index.php";
